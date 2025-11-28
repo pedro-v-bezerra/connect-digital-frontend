63f6fdf5e7df7b212a582dd34c700aee4e10deb9
Pedro Lima <pedrolima38@hotmail.com>
2025-11-27 22:06:58 -0300

    style: tailwindCSS config

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center rounded bg-red-500 p-4 text-white hover:bg-red-600">
+    <div className="bg-primary flex items-center justify-center rounded p-4 text-white hover:bg-red-600">
       Teste
     </div>
   )
